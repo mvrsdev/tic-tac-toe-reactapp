844544257982d53b08e568c11145206db9f39fd6
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-10-07 18:05:15 -0300

    Create reset button

---
+++ src/components/ResetButton.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const ResetContainer = styled.button`
+  border: none;
+  border-radius: 0.5rem;
+  color: #fff;
+  font-size: 2rem;
+  padding: 0.5rem 1rem;
+  margin: 2rem auto;
+  display: block;
+  background-color: #4287f5;
+  &:hover {
+    box-shadow: 0 0 8px #66a1ff;
+  }
+  &:active {
+    filter: brightness(85%);
+  }
+`;
+
+const ResetButton = ({ resetBoard }) => (
+  <ResetContainer onClick={resetBoard}>RESET</ResetContainer>
+);
+
+export default ResetButton;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Board } from './Board.js';
 export { default as Game } from './Game.js';
+export { default as ResetButton } from './ResetButton.js';
+export { default as ScoreBoard } from './ScoreBoard.js';
 export { default as Square } from './Square.js';
